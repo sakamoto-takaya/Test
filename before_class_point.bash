26d268ecb03698d97b04705eb2f065303853cb7b
sakamoto-takaya <takatakaya-no11@ezweb.ne.jp>
2020-11-06 08:26:00 +0000

    前回のポイント

--- before_class_point.bash
+++ before_class_point.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pushまでの手順はadd →　commit → push
-git pull で現在のリポジトリーに更新できる
-開発branchをつくり、そこで作業した結果をmasterにmergeする
+echo "pushまでの手順はadd →　commit → push"
+echo "git pull で現在のリポジトリーに更新できる"
+echo "開発branchをつくり、そこで作業した結果をmasterにmergeする"
